64c5c78e88dde0a31a8a665b52815437a127f942
Robert Christensen <robert.christensen@mymail.champlain.edu>
2021-02-24 22:48:18 -0500

    Portability and reliability fixes

--- config.py
+++ config.py
@@ -1,5 +1,24 @@
-#System-dependent values
+# Shortcut reading
 
-retroarch_path = r"D:\Programs\RetroArch\retroarch.exe"
-rom_path = r"D:\Programs\RetroArch\downloads\1G1R - NES\720 Degrees (USA).zip"
-core_path = r"D:\Programs\RetroArch\cores\mesen_libretro.dll"
+import win32com.client
+
+_shell = None
+def get_shortcut_target(path) -> str:
+	global _shell
+	if _shell == None:
+		_shell = win32com.client.Dispatch("WScript.Shell")
+	return _shell.CreateShortCut(path).Targetpath
+
+# Detect values
+
+import os.path
+
+data_path: str = get_shortcut_target(os.path.expanduser(r"~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\RetroArch\RetroArch Data Folder.lnk"))
+exe_path: str = get_shortcut_target(os.path.expanduser(r"~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\RetroArch\RetroArch.lnk"))
+
+rom_folder_path	: str = os.path.join(data_path, "downloads", "")
+core_folder_path: str = os.path.join(data_path, "cores", "")
+
+# Reference to the retroarch-config-overrides.cfg file
+
+config_overrides_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "retroarch-config-overrides.cfg")
--- tester.py
+++ tester.py
@@ -1,42 +1,34 @@
+from time import sleep
+import numpy # For image manipulation
+import imageio # For thumbnail generation
+from subprocess import Popen, PIPE
+from pywinauto.keyboard import send_keys as _send_keys
+from pywinauto.application import Application
+import os, os.path
+import config
+
 from enum import Enum
 class TestResult(Enum):
-    Unknown = 0
+    NotRun = 0
     Pass = 1
     Fail = 2
-    RequirementsNotMet = 3
-
-
-def safearg(s):
-    """Make an object safe by enclosing it in quotes"""
-    out = s
-    if out[0] != "\"":
-        out = "\""+out
-    if out[-1] != "\"":
-        out += "\""
-    return out
-
+    Inconclusive = 3
 
 def trailingslash(it):
     """Add a trailing slash if none exists"""
     return it if it[-1]=="/" or it[-1]=="\\" else it+"/"
 
 
-from pywinauto.application import Application
 def retroarch_launch(rom_path, core_path, retroarch_path):
     """Launch a RetroArch process and return the pywinauto wrapper"""
-    return Application().start(
-                      safearg(retroarch_path) # retroarch.exe
-              +" -L "+safearg(core_path)      # Specify core
-                 +" "+safearg(rom_path)       # Specify ROM
-    )
+    process = Popen([retroarch_path, "--appendconfig", config.config_overrides_path, "-L", core_path, rom_path], stdout=PIPE, stderr=PIPE)
+    return (process, Application().connect(process=process.pid))
 
 
-def take_screenshot(window):
+def take_screenshot(window) -> numpy.array:
     window.set_focus()
     return numpy.array(window.capture_as_image())[51:-8, 9:-8] # (8, 51) to (-8, -8) -> YY XX
 
-
-from pywinauto.keyboard import send_keys as _send_keys
 def send_input(key):
     _key = key[1:-1] if key[0] == "{" and key[-1] == "}" else key
     
@@ -44,80 +36,99 @@ def send_input(key):
     sleep(0.05)
     _send_keys("{"+_key+" up}")
 
-
-from time import sleep
-import numpy # For image manipulation
-from scipy.misc import imsave # For thumbnail generation
-def test(rom_path, core_path, retroarch_path, thumbnail_dir):
+def test_pair(rom_path, core_path, retroarch_path, thumbnail_dir:str=None, start_btn:str="{ENTER}", startup_time=2):
     """Run all tests for the given rom/core pair"""
 
-    rom_name  = ".".join(rom_path .split("\\")[-1].split(".")[:-1])
-    core_name = ".".join(core_path.split("\\")[-1].split(".")[:-1])
+    rom_name  : str = ".".join(rom_path .split("\\")[-1].split(".")[:-1])
+    core_name : str = ".".join(core_path.split("\\")[-1].split(".")[:-1])
     
-    retroarch = retroarch_launch(rom_path, core_path, retroarch_path)
+    proc: Popen
+    retroarch: Application
+    proc, retroarch = retroarch_launch(rom_path, core_path, retroarch_path)
     
-    results = {"load":     TestResult.Unknown,
-               "video":    TestResult.RequirementsNotMet,
-               "audio":    TestResult.RequirementsNotMet,
-               "controls": TestResult.RequirementsNotMet}
+    results = {"load":     TestResult.NotRun,
+               "video":    TestResult.NotRun,
+               "audio":    TestResult.NotRun,
+               "controls": TestResult.NotRun}
     
     sleep(0.25)
+    try:
+        
+        
+        ########################## TEST SECTION: LOAD ##########################
     
-    ########################## TEST SECTION: LOAD ##########################
+        #If RetroArch failed to start, report inconclusive
+        #If RetroArch CPU usage is below threshold, core probably failed to load
+        #If no windows opened, core probably failed to load
+        if not retroarch.is_process_running() or retroarch.cpu_usage(interval=2) < 0.2 or len(retroarch.windows()) < 1:
+            results["load"] = TestResult.Fail
+            return results
+        else:
+            results["load"] = TestResult.Pass
     
-    #If RetroArch failed to start, report inconclusive
-    if not retroarch.is_process_running():
-        results["load"] = TestResult.Fail
-        return results
-    #If RetroArch CPU usage is below threshold, core probably failed to load
-    elif retroarch.cpu_usage(interval=2) < 0.2:
-        results["load"] = TestResult.Fail
-        return results
-    #If no windows opened, core probably failed to load
-    elif len(retroarch.windows()) < 1:
-        results["load"] = TestResult.Fail
-        return results
-    else:
-        results["load"] = TestResult.Pass
+        retroarch_window = retroarch.top_window()
     
-    retroarch_window = retroarch.top_window()
+        #Just in case there are loading splashes...
+        retroarch_window.set_focus()
+        send_input("{ENTER}")
     
-    #Just in case there are loading splashes...
-    retroarch_window.set_focus()
-    send_input("{ENTER}")
+        #Wait 4 seconds for the modal to disappear
+        sleep(2)
     
-    #Wait 4 seconds for the modal to disappear
-    sleep(2)
+
+        ########################## TEST SECTION: VIDEO ##########################
+        
+        screencap_1: numpy.array
+        screencap_2: numpy.array
+
+        try:
+            #Do screengrab
+            screencap_1 = take_screenshot(retroarch_window)
+            sleep(startup_time) #Just in case there's a loading splash
+            screencap_2 = take_screenshot(retroarch_window)
     
-    ########################## TEST SECTION: VIDEO ##########################
+            #Save a thumbnail
+            if thumbnail_dir != None:
+                imageio.imwrite(trailingslash(thumbnail_dir)+rom_name+".png", screencap_2)
     
-    #Do screengrab
-    screencap_1 = take_screenshot(retroarch_window)
-    sleep(2) #Just in case there's a loading splash
-    screencap_2 = take_screenshot(retroarch_window)
+            #Check to see if the frame has anything in it, or if there is a significant difference (i.e. motion)
+            v = numpy.var((screencap_2-screencap_1).flatten()) + numpy.var(screencap_1.flatten()) + numpy.var(screencap_2.flatten())
+            if v > 1500:
+                results["video"] = TestResult.Pass
+            else:
+                results["video"] = TestResult.Fail
+                return results
+        except Exception as e:
+            results["video"] = TestResult.Inconclusive
+            return results
     
-    #Save a thumbnail
-    imsave(trailingslash(thumbnail_dir)+rom_name+".png", screencap_2)
+
+        ########################## TEST SECTION: CONTROLS ##########################
     
-    #Check to see if the frame has anything in it, or if there is a significant difference (i.e. motion)
-    v = numpy.var((screencap_2-screencap_1).flatten()) + numpy.var(screencap_1.flatten()) + numpy.var(screencap_2.flatten())
-    if v > 1500:
-        results["video"] = TestResult.Pass
-    else:
-        results["video"] = TestResult.Fail
-        return results
+        send_input(start_btn) #Start
+        sleep(2)
+        screencap_3: numpy.array = take_screenshot(retroarch_window)
+        v = numpy.var((screencap_3-screencap_2).flatten())
+        results["controls"] = TestResult.Pass if v > 1500 else TestResult.Fail # UNRELIABLE if splash screen is animated
     
-    ########################## TEST SECTION: CONTROLS ##########################
+
+        ########################## TEST SECTION: AUDIO ##########################
     
-    send_key("{ENTER}") #Start
-    sleep(2)
-    screencap_3 = take_screenshot(retroarch_window)
-    v = numpy.var((screencap_3-screencap_2).flatten())
-    results["controls"] = TestResult.Pass if v > 1500 else TestResult.Fail # UNRELIABLE if splash screen is animated
-    
-    ########################## TEST SECTION: AUDIO ##########################
-    
-    #Cleanup
-    retroarch.kill(soft=False)
+        # Not yet implemented...
+
+
+    finally:
+        #Cleanup
+        retroarch.kill(soft=False)
+        proc.kill()
     return results
 
+def test_dir(rom_dir, core_path, retroarch_path, thumbnail_dir:str=None, start_btn:str = "{ENTER}", startup_time=2):
+    roms: list[str] = [f for f in [os.path.join(rom_dir, f) for f in os.listdir(rom_dir)] if os.path.isfile(f)]
+    rv = {}
+    for rom_path in roms:
+        rv[rom_path] = test_pair(rom_path, core_path, retroarch_path, thumbnail_dir, start_btn, startup_time)
+    return rv
+
+if __name__ == "__main__":
+    raise AssertionError("Must be imported")
